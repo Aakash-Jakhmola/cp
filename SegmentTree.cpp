dc780df3ab1df051038b288bc4b57cca5ca2ddf9
aakashjakhmola <aakash.jakhmola@codingninjas.com>
2021-11-12 17:42:33 +0530

    refactored

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -8,14 +8,15 @@ class SegmentTree
 {
 	int size ; 
 	vector<int> seg ;
-	vector<int> mx ;
-	vector<int> mn ;
+
 	public : 
 	SegmentTree(int n) 
 	{
 		seg.assign(4*n,0) ; 
-		mx.assign(4*n, -inf) ; 
-		mn.assign(4*n, inf) ;
+	}
+
+	int fn(int x, int y) {
+		return x + y;
 	}
 	
 
@@ -23,8 +24,6 @@ class SegmentTree
 	{
 		if(l == r) {
 			seg[v] = a[l] ; 
-			mx[v] = a[l] ;
-			mn[v] = a[l] ; 
 			return ; 
 		}
 		if(l > r) 
@@ -32,9 +31,7 @@ class SegmentTree
 		int m = (l + r) / 2 ; 
 		build(v*2, l , m , a) ; 
 		build(v*2 + 1, m+1, r, a) ;
-		seg[v] = seg[v*2] + seg[v*2+1] ; 
-		mx[v] = max(mx[v*2] , mx[v*2 + 1]) ; 
-		mn[v] = min(mn[v*2] , mn[v*2 + 1]) ; 
+		seg[v] = fn( seg[v*2] , seg[v*2+1] ) ; 
 
 	}
 
@@ -50,9 +47,8 @@ class SegmentTree
 		} else {
 			update(v * 2 + 1, m + 1, r , idx , value) ;
 		}
-		seg[v] = seg[v*2] + seg[v*2 + 1] ; 
-		mx[v] = max(mx[v*2] , mx[v*2 + 1]) ; 
-		mn[v] = min(mn[v*2] , mn[v*2 + 1]) ; 
+		seg[v] = fn( seg[v*2] , seg[v*2+1] ) ; 
+		
 	}
 
 	int sum(int v, int l, int r, int ql, int qr) 
@@ -62,36 +58,12 @@ class SegmentTree
 		if(l >= ql && qr >= r)
 			return seg[v] ; 
 		int m = (l + r) / 2 ; 
-		int lsum = sum(v*2, l, m, ql, qr) ; 
-		int rsum = sum(v*2 + 1, m + 1, r, ql, qr) ; 
-		return lsum + rsum ;
+		int lval = sum(v*2, l, m, ql, qr) ; 
+		int rval = sum(v*2 + 1, m + 1, r, ql, qr) ; 
+		return fn(lval, rval);
 	}
 
-	int min_value(int v, int l, int r, int ql, int qr)
-	{
-	 
-		if(l > qr || r < ql )
-			return inf ; 
-		if(l >= ql && qr >= r)
-			return mn[v] ; 
-		int m = (l + r) / 2 ; 
-		int lmin = min_value(v*2, l, m, ql, qr) ; 
-		int rmin = min_value(v*2 + 1, m + 1, r, ql, qr) ; 
-		return min(lmin, rmin) ;
-	}
-
-	int max_value(int v, int l, int r, int ql, int qr) 
-	{
-		
-		if(l > qr  || r < ql)
-			return -inf ; 
-		if(l >= ql && qr >= r)
-			return mx[v] ; 
-		int m = (l + r) / 2 ; 
-		int lmax= max_value(v*2, l, m, ql, qr) ; 
-		int rmax = max_value(v*2 + 1, m + 1, r, ql , qr) ; 
-		return max(lmax, rmax) ;
-	}
+	
 };
 
 
