7dc4321dc4765c70ca9c56db5e35faeba9afc974
aakashjakhmola <aakash.jakhmola@codingninjas.com>
2021-12-12 14:14:39 +0530

    cleared git cache

--- gen.cpp
+++ gen.cpp
@@ -7,14 +7,14 @@ int rnd(int a, int b){
 int32_t main(int argc, char* argv[]){
     int seed = atoi(argv[1]);
     srand(seed);
-    int n = rnd(5,10);
-    cout<<n<<" ";
-    int k = rnd(5,1000000000) ;
-    cout << k << endl ;
+    cout << 1 << endl;
+    int n = rnd(1,100);
+    cout << n << endl;
+    int p = 0;
     for(int i = 0 ; i < n ; ++i) {
-        int x = rnd(1,100000000) ; int y = rnd(x+1,1000000000) ; 
-        int cost = rnd(1,1000000000) ; 
-        cout << x << " " << y << " " << cost <<"\n";
+        p +=  rnd(1, 100) ;
+        cout << p << " "; 
     }
-    cout<<endl;
+    return 0;
+    // cout<<endl;
 }
